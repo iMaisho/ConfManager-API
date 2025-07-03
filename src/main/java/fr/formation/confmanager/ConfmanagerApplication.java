3340800340ae384f6a014d47e1cf3e77355ef34d
iMaisho <antonin.mingam@gmail.com>
2025-07-03 16:02:56 +0200

    First commit

---
+++ src/main/java/fr/formation/confmanager/ConfmanagerApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.confmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/formation/confmanager/ConfmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.confmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
